d346abf63c86541b7a5d5e28382c393d7f28387c
Ibuki-Narukawa <ibuki0212job@gmail.com>
2022-05-22 15:14:15 +0900

    関数の型定義　学習ファイル追加

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,28 @@
 // const world = new World("Hello IBUKI!");
 // world.sayHello(root);
 
-// 03.基本の型定義
+// // 03.基本の型定義
+// import {
+//   anySample,
+//   notExistSample,
+//   primitiveSample,
+//   unknownSample,
+// } from "./basic";
+// unknownSample();
+// anySample();
+// notExistSample();
+// primitiveSample();
+
+// 04.関数の型定義
+import { logMessage } from "./function/basic";
 import {
-  anySample,
-  notExistSample,
-  primitiveSample,
-  unknownSample,
-} from "./basic";
-unknownSample();
-anySample();
-notExistSample();
-primitiveSample();
+  isUserSignedIn,
+  isUserSignedIn2,
+  sumProductsPrice,
+} from "./function/parameters";
+logMessage("Hello TypeScript!");
+isUserSignedIn("ABC", "ibuki");
+isUserSignedIn("DEF");
+isUserSignedIn2("ABC");
+const sum = sumProductsPrice(100, 200, 300, 400, 500);
+console.log("Function parameters sample 5:", sum);
